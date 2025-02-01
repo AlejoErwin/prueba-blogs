53ea2b1304e15f586b3dc99730eac21be766a47d
alejoErw <alejo.gutierrez.erwin@gmail.com>
2025-02-01 10:42:24 -0400

    se creo el proyecto base con java 21

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# prueba-blogs
+# prueba blogs
+## Crear el proyecto con los requerimientos
 Una pequeña API para registrar comentarios de blogs
---
+++ src/main/java/prueba/bisa/pruebablogs/PruebaBlogsApplication.java
@@ -0,0 +1,13 @@
+package prueba.bisa.pruebablogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBlogsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PruebaBlogsApplication.class, args);
+    }
+
+}
